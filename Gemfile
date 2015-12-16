84c7758e12f7d7dba0218e0eec1fbe24de6863a3
Jack Schuss <jackschuss@gmail.com>
2015-12-16 13:35:32 -0500

    set up factorygirl

--- Gemfile
+++ Gemfile
@@ -34,11 +34,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails', '~> 4.0'
 end
 
 group :development do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) {User.create!(email: "email@email.com", password: "password")}
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: user.id
       expect(response).to have_http_status(:success)
     end
   end
 
+  it "should return http status success after user signs in" do
+    pending "write code"
+  end
 
-  
+
+
+
+
+
 
 end
