908c620f3b3890e67ae4c665c41d21a2bd0b7f7c
Jack Schuss <jackschuss@gmail.com>
2015-12-17 15:13:33 -0500

    add item model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
+  get 'items/create'
+
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:create]
+  end
   root to: "welcome#index"
 end
