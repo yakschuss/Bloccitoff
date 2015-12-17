ee26120e6742475c102e0a8a366e62c477b292c4
Jack Schuss <jackschuss@gmail.com>
2015-12-17 17:06:15 -0500

    pass rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,29 @@ require 'rails_helper'
 
 
 RSpec.describe UsersController, type: :controller do
-  let(:user) {create(:user)}
-
+  let(:user) {create(:example_user, confirmed_at: Time.now)}
+context "for signed in users" do
   before do
+#    user.confirm
     sign_in user
   end
 
-  it "should return http status success after user signs in" do
-    pending "expect(response).to have_http_status :success"
+  it "returns http status success after user signs in" do
+     expect(response).to have_http_status :success
   end
 
-  it "should set current_user variable" do
-    pending "current_user.should_not be_nil"
+  it "sets current_user variable" do
+      expect(subject.current_user).to eq(user)
   end
 
-  it "should redirect to user#show" do
-    pending "expect(response).to redirect_to(user_show_path)"
+    context "GET #show" do
+    it "succeeds" do
+        get :show, id: user.id
+        expect(subject).to render_template(:show)
+    end
   end
 
+end
 
 
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
-  factory :user do
+  factory :example_user, class: User  do
     email "example@example.com"
     password "example123"
     password_confirmation "example123"
   end
+
+  # factory :user do
+  # email Faker::
+  # password
+  # password_confirmation
+#  end
 end
