91c606d3f906d1956801000cb1b33dcff9b81a5e
Jack Schuss <jackschuss@gmail.com>
2015-12-18 10:41:15 -0500

    add first_name last_name to users, sanitize parameters with custom registrations controller, add first_name, last_name to user#show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'items/create'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:show] do
     resources :items, only: [:create]
   end
