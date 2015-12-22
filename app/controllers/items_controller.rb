80dcd6df996db45181de9b7b4713346a0c684f40
Jack Schuss <jackschuss@gmail.com>
2015-12-21 19:47:39 -0500

    add item form, add to user#show view, start _item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+
   def create
+    @user = current_user
+    @item = @user.items.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Your item was saved correctly"
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "oops, something didn't work there."
+      redirect_to user_path(@user)
+    end
+
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:my_user){create(:user)}
+  let(:item) { create(:item, user: my_user) }
+
+
+   it { should belong_to(:user) }
+
+   it { should validate_presence_of(:name) }
+   it { should validate_length_of(:name).is_at_least(1) }
+
 end
