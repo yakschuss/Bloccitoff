7c6a9312670c2b0d40eeae0b931a89278817664d
Jack Schuss <jackschuss@gmail.com>
2015-12-23 12:01:35 -0500

    style user#show, add item#destroy stub

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:show] do
-    resources :items, only: [:create]
+    resources :items, only: [:create, :destroy]
   end
   root to: "welcome#index"
 end
