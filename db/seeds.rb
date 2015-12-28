edf32e341423e43104fb8901472065c53f2802bf
Jack Schuss <jackschuss@gmail.com>
2015-12-28 16:15:36 -0500

    add faker gem and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do
+    user = User.create!(
+
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+end
+
+
+
+users = User.all
+
+25.times do
+
+    item = Item.create!(
+      user: users.sample,
+      name: Faker::Lorem.sentence
+    )
+    item.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+end
+
+items = Item.all
+
+
+
+
+
+
+
+
+puts "Seed Done."
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
