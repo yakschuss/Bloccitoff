1a3908e7526751f37dd14f51d4781c353cb62c1a
Jack Schuss <jackschuss@gmail.com>
2015-12-30 12:47:46 -0500

    make db/seeds.rb idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,40 @@
 require 'faker'
 
-5.times do
-    user = User.create!(
-
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
-    )
-end
-
-
-
 users = User.all
 
-25.times do
 
-    item = Item.create!(
-      user: users.sample,
-      name: Faker::Lorem.sentence
-    )
-    item.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ if users.count == 5
+
+ else
+     until users.count == 5
+     User.create!(
+     first_name: Faker::Name.first_name,
+     last_name: Faker::Name.last_name,
+     email: Faker::Internet.email,
+     password: "password"
+     )
+    end
 
 end
 
+
+
 items = Item.all
+
+    if items.count >= 25
+
+    else
+        until items.count == 25
+
+        item = Item.create!(
+          user: users.sample,
+          name: Faker::Lorem.sentence
+        )
+        item.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+        end
+    end
+
+
 
 
 
