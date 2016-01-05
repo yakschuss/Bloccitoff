f725ef25ae7372a9f2329408556d801012a6458c
Jack Schuss <jackschuss@gmail.com>
2016-01-05 16:31:04 -0500

    delete cronjob test

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,3 @@ set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log
 every 1.day, :at => '12:00 am' do
   rake "todo:delete_items"
 end
-
-
-every 1.minute do
-  rake "say_hi:hi"
-end
